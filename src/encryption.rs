0949531f6e4110b0a647cc13f2e17c1ea3b224ee
Daniel <dculbert8136@gmail.com>
2026-01-10 21:34:34 -0500

    Added encrypt function.

--- src/encryption.rs
+++ src/encryption.rs
@@ -1,6 +1,6 @@
 use argon2::{password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, Salt, SaltString}, Algorithm, Argon2, Params, Version};
 use rand::{RngCore, rngs::OsRng};
-use aes_gcm::{aead::Nonce, Key};
+use aes_gcm::{AeadCore, Aes256Gcm, Key, KeyInit, aead::{Aead, Nonce}, aes::Aes256};
 use serde::Deserialize;
 use serde::Serialize;
 
@@ -47,13 +47,19 @@ pub struct EncryptionData {
  fn create_key_from_password_and_salt(password: &str, salt: &[u8]) -> [u8; 32] {
     let argon_parameters = Params::new(M_COST, T_COST, P_COST, Some(OUTPUT_LEN)).expect("Error creating argon parameters.");
     let argon = Argon2::new(Algorithm::Argon2id, Version::V0x13, argon_parameters);
-    let mut key =[0u8; 32];
+    let mut key = [0u8; 32];
     argon.hash_password_into(password.as_bytes(), salt, &mut key).expect("Error with argon hasing password into key");
     return key;
 }
 
-fn create_ciphertext_from_key_and_password(key: &[u8], password: &str) {
-    let mut nonce_bytes = [0u8; 12];
-    OsRng.fill_bytes(&mut nonce_bytes);
-    let nonce = Nonce.from_slice(&nonce_bytes);
+
+fn encrypt(plaintext: &str) {
+    let mut salt = [0u8; 32];
+    OsRng.fill_bytes(&mut salt);
+    let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+    let key = create_key_from_password_and_salt(&plaintext, &salt);
+    let cipher = Aes256Gcm::new_from_slice(&key).expect("Error initializng cipher.");
+    let plaintext_bytes = plaintext.as_bytes();
+    let ciphertext = cipher.encrypt(&nonce, plaintext_bytes).expect("Error creating cyphertext from nonce and plaintext bytes.");
+    println!("{:#?}", ciphertext);
 }
