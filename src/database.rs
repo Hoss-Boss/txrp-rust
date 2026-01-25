f20dbecbb6a336d02ac702e0c2a077a834966cd3
Daniel <dculbert8136@gmail.com>
2026-01-24 19:45:47 -0500

    Startup function initializes EncryptionData DB table.

--- src/database.rs
+++ src/database.rs
@@ -11,7 +11,27 @@ pub fn apply_schema_to_db_file() {
     )
     ".to_string();
     let connection = rusqlite::Connection::open(file).expect("Error opening wallets.db");
+    let query_2 = "CREATE TABLE IF NOT EXISTS `EncryptionData` 
+    (`ID` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `WalletID` INTEGER NOT NULL,
+    `MnemonicSalt` TEXT NOT NULL,
+    `MnemonicNonce` TEXT NOT NULL,
+    `SeedSalt` TEXT NOT NULL,
+    `SeedNonce` TEXT NOT NULL,
+    `M_COST` INTEGER NOT NULL,
+    `T_COST` INTEGER NOT NULL,
+    `P_COST` INTEGER NOT NULL,
+    `OUTPUT_LEN` INTEGER NOT NULL,
+    `ArgonAlgorithm` TEXT NOT NULL,
+    `ArgonVersion` TEXT NOT NULL,
+
+    FOREIGN KEY (WalletID) REFERENCES Wallet(ID)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+    )
+    ".to_string();
     connection.execute(&query, []).expect("Error writing to wallets.db");
+    connection.execute(&query_2, []).expect("Error writing to wallets.db");
 }
 
 pub fn attempt_test_data_insertion() {
